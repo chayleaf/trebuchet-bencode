abc338e79991af8110e875f7cbb5c6aa1f9b108a
Ivan Pavluk <chayleaf@protonmail.com>
2021-09-09 03:56:20 +0700

    add tests, docs and readme; proper release

---
+++ README.md
@@ -0,0 +1,5 @@
+# Trebuchet Bencode
+
+A crate for parsing [Bencode](https://en.wikipedia.org/wiki/Bencode) made with [nom](https://docs.rs/nom)
+
+Custom data types with custom parse errors can be defined as well. Nearly-zero-alloc deserializing is also possible (Only lists have to allocate).
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
+/// A crate for [Bencoding](https://en.wikipedia.org/wiki/Bencode)
 use itertools::Itertools;
+pub use nom;
 use nom::{
     branch::alt,
     bytes::complete::{tag, take, take_while1},
@@ -15,9 +17,28 @@ use std::{
     str,
 };
 
-pub trait Bdecodable<'a>: Sized + 'a {
+/// A trait to be implemented by any type that can be encoded and decoded using Bencoding
+pub trait Bencodable<'a>: Sized + 'a {
+    /// The error type returned by the bdecode function.
+    /// Use `type Error = nom::error::Error<&'a [u8]>;` as the default (default associated types
+    /// aren't supported yet).
     type Error: ParseError<&'a [u8]>;
+    /// Deserialize from bencoded data.
+    ///
+    /// ```
+    /// # use trebuchet_bencode::Bencodable;
+    /// let input = b"i5ei6enon-bencoded data";
+    /// // Parse a bencoded value, get leftover data and the value
+    /// let (input, a) = i32::bdecode(input)?;
+    /// // Parse one more value
+    /// let (input, b) = i32::bdecode(input)?;
+    /// assert_eq!(a, 5);
+    /// assert_eq!(b, 6);
+    /// assert_eq!(input, b"non-bencoded data");
+    /// # Ok::<(), nom::Err<<i32 as Bencodable>::Error>>(())
+    /// ```
     fn bdecode(input: &'a [u8]) -> IResult<&'a [u8], Self, Self::Error>;
+    /// Serialize into bencoding.
     fn bencode<W: Write>(&self, stream: &mut W) -> io::Result<()>;
 }
 
@@ -34,28 +55,30 @@ fn decimal<T: PrimInt>(input: &[u8]) -> IResult<&[u8], T> {
 }
 
 fn bdecode_int<T: PrimInt>(input: &[u8]) -> IResult<&[u8], T> {
-    preceded(tag(b"i"), decimal)(input)
+    delimited(tag(b"i"), decimal, tag(b"e"))(input)
 }
 
 macro_rules! int_impl {
     ($($t:ty)*) => {$(
-        impl<'a> Bdecodable<'a> for $t {
+        impl<'a> Bencodable<'a> for $t {
             type Error = nom::error::Error<&'a [u8]>;
             fn bdecode(input: &'a [u8]) -> IResult<&'a [u8], Self> {
                 bdecode_int(input)
             }
             fn bencode<W: Write>(&self, stream: &mut W) -> io::Result<()> {
-                stream.write_all(self.to_string().as_ref())
+                stream.write_all(b"i")?;
+                stream.write_all(self.to_string().as_ref())?;
+                stream.write_all(b"e")
             }
         }
-    )*}
+    )*};
 }
 
 // u8 isn't implemented not to conflict with Vec<u8> as owned byte array...
 // not the best solution is it...
 int_impl!(i8 i16 i32 i64 i128 isize u16 u32 u64 u128 usize);
 
-impl<'a> Bdecodable<'a> for &'a [u8] {
+impl<'a> Bencodable<'a> for &'a [u8] {
     type Error = nom::error::Error<&'a [u8]>;
     fn bdecode(input: &'a [u8]) -> IResult<&'a [u8], Self> {
         let (input, len) = decimal::<usize>(input)?;
@@ -68,7 +91,7 @@ impl<'a> Bdecodable<'a> for &'a [u8] {
     }
 }
 
-impl<'a> Bdecodable<'a> for Vec<u8> {
+impl<'a> Bencodable<'a> for Vec<u8> {
     type Error = nom::error::Error<&'a [u8]>;
     fn bdecode(input: &'a [u8]) -> IResult<&'a [u8], Self> {
         map(<&'a [u8]>::bdecode, From::from)(input)
@@ -78,7 +101,7 @@ impl<'a> Bdecodable<'a> for Vec<u8> {
     }
 }
 
-impl<'a, T: Bdecodable<'a>> Bdecodable<'a> for Vec<T> {
+impl<'a, T: Bencodable<'a>> Bencodable<'a> for Vec<T> {
     type Error = T::Error;
     fn bdecode(input: &'a [u8]) -> IResult<&'a [u8], Self, Self::Error> {
         delimited(tag(b"l"), many0(T::bdecode), tag(b"e"))(input)
@@ -90,158 +113,210 @@ impl<'a, T: Bdecodable<'a>> Bdecodable<'a> for Vec<T> {
     }
 }
 
-impl<'a, T: Bdecodable<'a>> Bdecodable<'a> for HashMap<&'a [u8], T>
-where
-    T::Error: From<nom::error::Error<&'a [u8]>>,
-{
-    type Error = T::Error;
-    fn bdecode(input: &'a [u8]) -> IResult<&'a [u8], Self, Self::Error> {
-        delimited(
-            tag(b"d"),
-            |input| {
-                let mut it = iterator(
-                    input,
-                    pair(
-                        |input| <&'a [u8]>::bdecode(input).map_err(nom::Err::convert),
-                        T::bdecode,
-                    ),
-                );
-                let ret = it.collect::<Self>();
-                let (input, _) = it.finish()?;
-                Ok((input, ret))
-            },
-            tag(b"e"),
-        )(input)
-    }
-    fn bencode<W: Write>(&self, stream: &mut W) -> io::Result<()> {
-        stream.write_all(b"d")?;
-        self.iter()
-            .sorted_by(|(a, _), (b, _)| Ord::cmp(a, b))
-            .try_for_each(|(k, v)| k.bencode(stream).and_then(|_| v.bencode(stream)))?;
-        stream.write_all(b"e")
-    }
+macro_rules! impl_hashmap {
+    ($($ty:ty)*) => {$(
+        impl<'a, T: Bencodable<'a>> Bencodable<'a> for $ty
+        where
+            T::Error: From<nom::error::Error<&'a [u8]>>,
+        {
+            type Error = T::Error;
+            fn bdecode(input: &'a [u8]) -> IResult<&'a [u8], Self, Self::Error> {
+                delimited(
+                    tag(b"d"),
+                    |input| {
+                        let mut it = iterator(
+                            input,
+                            pair(
+                                |input| <&'a [u8]>::bdecode(input).map_err(nom::Err::convert),
+                                T::bdecode,
+                            ),
+                        );
+                        let ret = it.map(|(k, v)| (k.into(), v)).collect::<Self>();
+                        let (input, _) = it.finish()?;
+                        Ok((input, ret))
+                    },
+                    tag(b"e"),
+                )(input)
+            }
+            fn bencode<W: Write>(&self, stream: &mut W) -> io::Result<()> {
+                stream.write_all(b"d")?;
+                self.iter()
+                    .sorted_by(|(a, _), (b, _)| Ord::cmp(a, b))
+                    .try_for_each(|(k, v)| k.bencode(stream).and_then(|_| v.bencode(stream)))?;
+                stream.write_all(b"e")
+            }
+        }
+    )*};
 }
 
-impl<'a, T: Bdecodable<'a>> Bdecodable<'a> for HashMap<Vec<u8>, T>
-where
-    T::Error: From<nom::error::Error<&'a [u8]>>,
-{
-    type Error = T::Error;
-    fn bdecode(input: &'a [u8]) -> IResult<&'a [u8], Self, Self::Error> {
-        delimited(
-            tag(b"d"),
-            |input| {
-                let mut it = iterator(
-                    input,
-                    pair(
-                        |input| <&'a [u8]>::bdecode(input).map_err(nom::Err::convert),
-                        T::bdecode,
-                    ),
-                );
-                let ret = it.map(|(k, v)| (k.to_owned(), v)).collect::<Self>();
-                let (input, _) = it.finish()?;
-                Ok((input, ret))
-            },
-            tag(b"e"),
-        )(input)
-    }
-    fn bencode<W: Write>(&self, stream: &mut W) -> io::Result<()> {
-        stream.write_all(b"d")?;
-        self.iter()
-            .sorted_by(|(a, _), (b, _)| Ord::cmp(a, b))
-            .try_for_each(|(k, v)| k.bencode(stream).and_then(|_| v.bencode(stream)))?;
-        stream.write_all(b"e")
-    }
-}
+impl_hashmap!(HashMap<Vec<u8>, T> HashMap<&'a [u8], T>);
 
+/// An enum representing any bencoded datatype with a lifetime
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub enum BdecodeAny<'a> {
+pub enum BencodeAny<'a> {
     Int(i64),
-    List(Vec<BdecodeAny<'a>>),
-    Dict(HashMap<&'a [u8], BdecodeAny<'a>>),
+    List(Vec<BencodeAny<'a>>),
+    Dict(HashMap<&'a [u8], BencodeAny<'a>>),
     Str(&'a [u8]),
 }
 
+/// An enum representing any bencoded datatype (owned)
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub enum BdecodeAnyOwned {
+pub enum BencodeAnyOwned {
     Int(i64),
-    List(Vec<BdecodeAnyOwned>),
-    Dict(HashMap<Vec<u8>, BdecodeAnyOwned>),
+    List(Vec<BencodeAnyOwned>),
+    Dict(HashMap<Vec<u8>, BencodeAnyOwned>),
     Str(Vec<u8>),
 }
 
-impl<'a> From<&'a BdecodeAny<'a>> for BdecodeAnyOwned {
-    fn from(val: &'a BdecodeAny<'a>) -> Self {
+impl<'a> From<&'a BencodeAny<'a>> for BencodeAnyOwned {
+    fn from(val: &'a BencodeAny<'a>) -> Self {
         match val {
-            BdecodeAny::Int(n) => Self::Int(*n),
-            BdecodeAny::List(v) => Self::List(v.iter().map(From::from).collect()),
-            BdecodeAny::Dict(d) => Self::Dict(
+            BencodeAny::Int(n) => Self::Int(*n),
+            BencodeAny::List(v) => Self::List(v.iter().map(From::from).collect()),
+            BencodeAny::Dict(d) => Self::Dict(
                 d.iter()
                     .map(|(&k, v)| (k.to_owned(), From::from(v)))
                     .collect(),
             ),
-            BdecodeAny::Str(s) => Self::Str(s.to_vec()),
+            BencodeAny::Str(s) => Self::Str(s.to_vec()),
         }
     }
 }
 
-impl<'a> From<BdecodeAny<'a>> for BdecodeAnyOwned {
-    fn from(val: BdecodeAny<'a>) -> Self {
+impl<'a> From<BencodeAny<'a>> for BencodeAnyOwned {
+    fn from(val: BencodeAny<'a>) -> Self {
         Self::from(&val)
     }
 }
 
-impl<'a> BdecodeAny<'a> {
-    pub fn into_owned(&self) -> BdecodeAnyOwned {
+impl<'a> BencodeAny<'a> {
+    /// Creates an owned object from a borrowed object.
+    pub fn into_owned(&self) -> BencodeAnyOwned {
         From::from(self)
     }
 }
 
-impl<'a> Bdecodable<'a> for BdecodeAny<'a> {
-    type Error = nom::error::Error<&'a [u8]>;
-    fn bdecode(input: &'a [u8]) -> IResult<&'a [u8], Self> {
-        alt((
-            map(i64::bdecode, Self::Int),
-            map(Vec::<BdecodeAny>::bdecode, Self::List),
-            map(HashMap::<&[u8], BdecodeAny>::bdecode, Self::Dict),
-            map(<&'a [u8]>::bdecode, Self::Str),
-        ))(input)
-    }
-    fn bencode<W: Write>(&self, stream: &mut W) -> io::Result<()> {
-        match self {
-            Self::Int(n) => n.bencode(stream),
-            Self::List(v) => v.bencode(stream),
-            Self::Dict(d) => d.bencode(stream),
-            Self::Str(s) => s.bencode(stream),
+macro_rules! impl_any {
+    ($($ty:ty)*) => {$(
+        impl<'a> Bencodable<'a> for $ty {
+            type Error = nom::error::Error<&'a [u8]>;
+            fn bdecode(input: &'a [u8]) -> IResult<&'a [u8], Self> {
+                alt((
+                    map(Bencodable::bdecode, Self::Int),
+                    map(Bencodable::bdecode, Self::List),
+                    map(Bencodable::bdecode, Self::Dict),
+                    map(Bencodable::bdecode, Self::Str),
+                ))(input)
+            }
+            fn bencode<W: Write>(&self, stream: &mut W) -> io::Result<()> {
+                match self {
+                    Self::Int(x) => x.bencode(stream),
+                    Self::List(x) => x.bencode(stream),
+                    Self::Dict(x) => x.bencode(stream),
+                    Self::Str(x) => x.bencode(stream),
+                }
+            }
         }
-    }
+    )*};
 }
 
-impl<'a> Bdecodable<'a> for BdecodeAnyOwned {
-    type Error = nom::error::Error<&'a [u8]>;
-    fn bdecode(input: &'a [u8]) -> IResult<&'a [u8], Self> {
-        alt((
-            map(i64::bdecode, Self::Int),
-            map(Vec::<BdecodeAnyOwned>::bdecode, Self::List),
-            map(HashMap::<Vec<u8>, BdecodeAnyOwned>::bdecode, Self::Dict),
-            map(Vec::<u8>::bdecode, Self::Str),
-        ))(input)
-    }
-    fn bencode<W: Write>(&self, stream: &mut W) -> io::Result<()> {
-        match self {
-            Self::Int(n) => n.bencode(stream),
-            Self::List(v) => v.bencode(stream),
-            Self::Dict(d) => d.bencode(stream),
-            Self::Str(s) => s.bencode(stream),
-        }
-    }
-}
+impl_any!(BencodeAny<'a> BencodeAnyOwned);
 
-// TODO!
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::fmt::Debug;
+
+    fn s(x: &[u8]) -> &[u8] {
+        x
+    }
+
+    fn verify1<'a, T: Debug + std::cmp::PartialEq + Bencodable<'a>>(
+        test: &'a [u8],
+        expected: &T,
+        expected_back: Option<&'a [u8]>,
+        leftover: Option<&'a [u8]>,
+    ) where
+        T::Error: Debug,
+    {
+        let (input, dec) = T::bdecode(test).unwrap();
+        assert_eq!(&dec, expected);
+        assert_eq!(input, leftover.unwrap_or(b""));
+        let mut out = Vec::<u8>::new();
+        dec.bencode(&mut out).unwrap();
+        assert_eq!(out, expected_back.unwrap_or(test));
+    }
+
+    fn verify<'a, T: Debug + std::cmp::PartialEq + Bencodable<'a>>(test: &'a [u8], expected: &T)
+    where
+        T::Error: Debug,
+    {
+        verify1(test, expected, None, None)
+    }
+
+    fn ensure_fail<'a, T: Bencodable<'a>>(test: &'a [u8]) {
+        assert!(T::bdecode(test).is_err());
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn int_values() {
+        verify(b"i-6234e", &-6234);
+        ensure_fail::<i64>(b"i-6234");
+        ensure_fail::<u16>(b"i65536e");
+        ensure_fail::<u64>(b"i-6234e");
+        verify(b"i18446744073709551615e", &18446744073709551615u64);
+        verify(b"i-9223372036854775808e", &-9223372036854775808i64);
+    }
+
+    #[test]
+    fn string_values() {
+        verify(b"4:abcd", &s(b"abcd"));
+        verify(b"4:abcd", &vec![b'a', b'b', b'c', b'd']);
+        ensure_fail::<Vec<u8>>(b"5:abcd");
+        verify1(b"3:abcdef", &s(b"abc"), Some(b"3:abc"), Some(b"def"));
+        verify(b"0:", &s(b""));
+        ensure_fail::<Vec<u8>>(b":abcd");
+    }
+
+    #[test]
+    fn list_values() {
+        // Be careful not to use Vec<u8>, as that's the string type!
+        verify1(b"li1ei2ei3eee", &vec![1, 2, 3], Some(b"li1ei2ei3ee"), Some(b"e"));
+        verify1(
+            b"l4:spam4:eggsi5eei5e",
+            &vec![
+                BencodeAny::Str(b"spam"),
+                BencodeAny::Str(b"eggs"),
+                BencodeAny::Int(5),
+            ],
+            Some(b"l4:spam4:eggsi5ee"),
+            Some(b"i5e"),
+        );
+        verify(b"le", &Vec::<u16>::new());
+        ensure_fail::<Vec<u16>>(b"e");
+        ensure_fail::<Vec<u16>>(b"li65536ei65536ee");
+    }
+
+    #[test]
+    fn dict_values() {
+        verify1(
+            b"d4:testi1337e4:spaml1:a1:beee",
+            &vec![
+                (s(b"test"), BencodeAny::Int(1337)),
+                (
+                    s(b"spam"),
+                    BencodeAny::List(vec![BencodeAny::Str(b"a"), BencodeAny::Str(b"b")]),
+                ),
+            ]
+            .drain(..)
+            .collect::<HashMap<&[u8], BencodeAny>>(),
+            Some(b"d4:spaml1:a1:be4:testi1337ee"), // Serialization must be sorted
+            Some(b"e"),
+        );
+        ensure_fail::<HashMap<&[u8], BencodeAny>>(b"d4:spaml1:a1:be4:testti1337ee");
+        ensure_fail::<HashMap<&[u8], BencodeAny>>(b"l4:spaml1:a1:be4:testi1337ee");
+        verify(b"de", &HashMap::<&[u8], u16>::new());
+        ensure_fail::<HashMap<&[u8], BencodeAny>>(b"dd4:spaml1:a1:be4:testi1337ee");
     }
 }
